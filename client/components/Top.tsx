08c1e28972f8c419d6737cf5ea5a2aa04bca83d3
azusanakahashi <azupin86@gmail.com>
2022-09-28 21:13:42 -0700

    add sass and style top page

--- client/components/Top.tsx
+++ client/components/Top.tsx
@@ -1,18 +1,19 @@
 import text from "../public/text";
 import Image from "next/image";
 import topImage from "../public/pictures/top.jpeg";
+import styles from "../styles/top.module.scss";
 
 const Top = () => {
   return (
-    <div className="top">
-      <div className="left">
+    <div className={styles["top"]}>
+      <div className={styles["left"]}>
         <h1>Azusa Nakahashi</h1>
         <h2>Front-end developer</h2>
         {text.data.top.map((text, key) => (
           <p key={key}>{text}</p>
         ))}
       </div>
-      <div className="right">
+      <div className={styles["right"]}>
         <Image src={topImage} alt="facePicture" />
       </div>
     </div>
