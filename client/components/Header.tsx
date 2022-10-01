9e626fc6a3acbdce2709a4974800bc388b99a519
azusanakahashi <azupin86@gmail.com>
2022-09-30 18:52:39 -0700

    style header nav

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,35 +1,55 @@
 import { useAppSelector, useAppDispatch } from "../pages/app/hooks";
 import { navToggle } from "../features/buttons";
 import styles from "../styles/header.module.scss";
+import Image from "next/image";
+import hamburger from "../public/icons/hamburger.svg";
 
 const Header = () => {
   const navIsOpen = useAppSelector((state) => state.buttons.navIsOpen);
   const dispatch = useAppDispatch();
   return (
-    <header
-      className={`${styles["header"]} ${
-        navIsOpen ? styles["open"] : styles["close"]
-      }`}
-    >
-      <div className={styles["nav-close-button"]}>
-        <p className={styles["logo"]}>Logo</p>
-        <p
-          className={navIsOpen ? styles["open"] : styles["close"]}
-          onClick={() => dispatch(navToggle())}
+    <header className={`${styles["header"]} ${!navIsOpen && styles["close"]}`}>
+      <nav>
+        <div className={styles["logo-and-buttons"]}>
+          <p className={styles["logo"]}>Logo</p>
+          <div className={styles["toggle-buttons"]}>
+            <div
+              className={`${styles["hamburger"]} ${
+                !navIsOpen && styles["close"]
+              }`}
+            >
+              <Image
+                src={hamburger}
+                alt="hamburger icon"
+                onClick={() => dispatch(navToggle())}
+              />
+            </div>
+            <p
+              className={`${styles["close-button"]} ${
+                !navIsOpen && styles["close"]
+              }`}
+              onClick={() => dispatch(navToggle())}
+            >
+              close
+            </p>
+          </div>
+        </div>
+        <div
+          className={`${styles["page-section-list"]} ${
+            !navIsOpen && styles["close"]
+          }`}
         >
-          close
-        </p>
-      </div>
-      <nav className={navIsOpen ? styles["open"] : styles["close"]}>
-        <ul>
-          <li>About</li>
-          <li>Projects</li>
-          <li>Contact</li>
-        </ul>
-        <ul>
-          <li>linkedIn</li>
-          <li>Git</li>
-        </ul>
+          <ul>
+            <li>navIsOpen {navIsOpen ? "true" : "false"}</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+          <ul>
+            <li>linkedIn</li>
+            <li>Git</li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
--- client/components/Top.tsx
+++ client/components/Top.tsx
@@ -1,23 +1,14 @@
 import text from "../public/text";
 import Image from "next/image";
 import topImage from "../public/pictures/top.jpeg";
-import hamburger from "../public/icons/hamburger.svg";
 import styles from "../styles/top.module.scss";
 import { useAppSelector, useAppDispatch } from "../pages/app/hooks";
-import { navToggle } from "../features/buttons";
 
 const Top = () => {
   const navIsOpen = useAppSelector((state) => state.buttons.navIsOpen);
   const dispatch = useAppDispatch();
   return (
     <div className={styles["topPage"]}>
-      <div className={styles["hamburger"]}>
-        <Image
-          src={hamburger}
-          alt="hamburger icon"
-          onClick={() => dispatch(navToggle())}
-        />
-      </div>
       <div className={styles["left"]}>
         <h1>Azusa Nakahashi</h1>
         <h2>Front-end developer</h2>
