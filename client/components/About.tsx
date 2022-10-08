a4ad5ccaa29eec2b010f77a9e373ebf3ed1ebc22
azusanakahashi <azupin86@gmail.com>
2022-10-07 21:09:04 -0700

    style about section

--- client/components/About.tsx
+++ client/components/About.tsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import text from "../public/text";
 import styles from "../styles/about.module.scss";
 const About = () => {
+  const [openPage, setOpenPage] = useState(0);
   return (
     <div className={styles["about"]} id="about">
       <h2>About</h2>
-      <div className="text-container">
-        <div className="text">
+      <div className={styles["text-container"]}>
+        <div className={styles["text"]}>
           {text.data.about.map((page, key) => {
             return (
-              <div key={key} className={`page-${key}`}>
+              <div
+                key={key}
+                className={`${styles[`page-${key}`]}  ${
+                  key === openPage ? styles["open"] : styles["close"]
+                }`}
+              >
                 {page.map((paragraph, key) => {
                   return <p key={key}>{paragraph}</p>;
                 })}
@@ -16,8 +23,33 @@ const About = () => {
             );
           })}
         </div>
-        <p onClick={() => console.log("this can be dots")}>next page → </p>
-        <p>and more...</p>
+        <div className={styles["page-controller"]}>
+          <p
+            className={`${styles["previous-btn"]} ${
+              openPage === 0 ? styles["close"] : styles["open"]
+            }`}
+            onClick={() => {
+              console.log("clicked");
+              if (openPage === 1) {
+                setOpenPage(openPage - 1);
+              }
+            }}
+          >
+            {"← previous page"}
+          </p>{" "}
+          <p
+            className={`${styles["next-btn"]} ${
+              openPage === 0 ? styles["open"] : styles["close"]
+            }`}
+            onClick={() => {
+              if (openPage === 0) {
+                setOpenPage(openPage + 1);
+              }
+            }}
+          >
+            {"next page →"}
+          </p>
+        </div>
       </div>
       <div className="skills"></div>
     </div>
