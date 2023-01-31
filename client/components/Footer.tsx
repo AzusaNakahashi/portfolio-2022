d437afab5ab673f28b4f5cdcad163add06665724
azusanakahashi <azupin86@gmail.com>
2023-01-31 14:58:14 -0800

    change footer color to navy

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <h3>Designed and buit by Azusa Nakahashi 2022</h3>
+      <h3>Designed and buit by &copy;Azusa Nakahashi 2022</h3>
     </footer>
   );
 };
